46be866fa4d3029e474f9f14c29157a58c26dea9
Yoon Ji Hoon <wlgns7922@gmail.com>
2026-01-26 15:30:52 +0900

    Deferred rendering 구현 완료

--- Engine/Shader.h
+++ Engine/Shader.h
@@ -5,6 +5,7 @@ enum class SHADER_TYPE : uint8
 {
 	DEFERRED,
 	FORWARD,
+	LIGHTING,
 };
 
 enum class RASTERIZER_TYPE
@@ -21,6 +22,17 @@ enum class DEPTH_STENCIL_TYPE
 	LESS_EQUAL,
 	GREATER,
 	GREATER_EQUAL,
+	NO_DEPTH_TEST,	// 깊이 테스트(X) + 깊이 기록(O)
+	NO_DEPTH_TEST_NO_WRITE,	// 깊이 테스트(X) + 깊이 기록(X)
+	LESS_NO_WRITE,	// 깊이 테스트(O) + 깊이 기록(X)
+};
+
+enum class BLEND_TYPE : uint8
+{
+	DEFAULT,
+	ALPHA_BLEND,
+	ONE_TO_ONE_BLEND,
+	END,
 };
 
 struct ShaderInfo
@@ -28,6 +40,7 @@ struct ShaderInfo
 	SHADER_TYPE shaderType = SHADER_TYPE::FORWARD;
 	RASTERIZER_TYPE rasterizerType = RASTERIZER_TYPE::CULL_BACK;
 	DEPTH_STENCIL_TYPE depthStencilType = DEPTH_STENCIL_TYPE::LESS;
+	BLEND_TYPE blendType = BLEND_TYPE::DEFAULT;
 	D3D12_PRIMITIVE_TOPOLOGY_TYPE topologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
 };
 
@@ -37,7 +50,7 @@ public:
 	Shader();
 	virtual ~Shader();
 
-	void Init(const wstring& path, ShaderInfo info = ShaderInfo());
+	void Init(const wstring& path, ShaderInfo info = ShaderInfo(), const string& vs = "VS_Main", const string& ps = "PS_Main");
 	void Update();
 
 	SHADER_TYPE GetShaderType() { return _info.shaderType; }
