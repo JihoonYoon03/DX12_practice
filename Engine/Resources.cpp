891e08e4b6d6b01849b1601667c49046a5717dd5
Yoon Ji Hoon <wlgns7922@gmail.com>
2026-01-29 14:38:24 +0900

    Instancing 추가

--- Engine/Resources.cpp
+++ Engine/Resources.cpp
@@ -465,4 +465,16 @@ void Resources::CreateDefaultMaterial()
 
 		Add<Material>(L"ComputeParticle", material);
 	}
+
+	// GameObject
+	{
+		shared_ptr<Shader> shader = GET_SINGLE(Resources)->Get<Shader>(L"Deferred");
+		shared_ptr<Texture> texture = GET_SINGLE(Resources)->Load<Texture>(L"Tile", L"..\\Resources\\Texture\\Tile.png");
+		shared_ptr<Texture> texture2 = GET_SINGLE(Resources)->Load<Texture>(L"Tile_Normal", L"..\\Resources\\Texture\\Tile_Normal.png");
+		shared_ptr<Material> material = make_shared<Material>();
+		material->SetShader(shader);
+		material->SetTexture(0, texture);
+		material->SetTexture(1, texture2);
+		Add<Material>(L"GameObject", material);
+	}
 }
