63ae995771b79230c7189d77db1adc2176c2927b
Yoon Ji Hoon <wlgns7922@gmail.com>
2026-02-03 12:31:36 +0900

    FBX 파일 불러오기 추가

--- Engine/MeshRenderer.h
+++ Engine/MeshRenderer.h
@@ -19,10 +19,10 @@ public:
 	MeshRenderer();
 	virtual ~MeshRenderer();
 
-	shared_ptr<Material> GetMaterial() { return _material; }
+	shared_ptr<Material> GetMaterial(uint32 idx = 0) { return _materials[idx]; }
 
 	void SetMesh(shared_ptr<Mesh> mesh) { _mesh = mesh; }
-	void SetMaterial(shared_ptr<Material> material) { _material = material; }
+	void SetMaterial(shared_ptr<Material> material, uint32 idx = 0);
 
 	void Render();
 	void Render(shared_ptr<class InstancingBuffer>& buffer);
@@ -32,6 +32,6 @@ public:
 
 private:
 	shared_ptr<Mesh> _mesh;
-	shared_ptr<Material> _material;
+	vector<shared_ptr<Material>> _materials;
 };
 
